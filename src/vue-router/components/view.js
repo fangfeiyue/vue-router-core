3d9e0cd0b6712bbee4ef1350df0468de1625b3f6
FangFeiyue <18766131540@163.com>
2021-03-14 18:17:37 +0800

    猜你喜欢

--- src/vue-router/components/view.js
+++ src/vue-router/components/view.js
@@ -5,7 +5,21 @@ export default {
     let route = parent.$route
     let depth = 0
     let records = route.matched
+    data.routerView = true // 渲染router-view时标记它是一个router-view
 
-    return h(records[depth].component)
+    while(parent){ // 根据matched 渲染对应的router-view
+      if (parent.$vnode && parent.$vnode.data.routerView){
+          depth++;
+      }
+      parent = parent.$parent;
+    }
+
+    let record = route.matched[depth];
+    // 如果没有匹配的记录渲染空值
+    if(!record){
+        return h();
+    }
+
+    return h(records[depth].component, data)
   }
 }
